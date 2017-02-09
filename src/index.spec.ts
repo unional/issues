2e2ba1a6d816607493ec1083dc4c9ef03ff50634
Homa Wong <homawong@gmail.com>
2017-02-09 02:25:01 -0800

    Update test to use jsdomify

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,17 @@
 import test from 'ava'
 import * as SystemJS from 'systemjs'
+import jsdomify from 'jsdomify'
+
+test.before(() => {
+  jsdomify.create()
+})
+
+test.after(() => {
+  jsdomify.desctroy()
+})
 
 test('npm-module', async _t => {
+  console.log('isBrowser', typeof window !== 'undefined' && typeof document !== 'undefined')
   const pluginName = 'npm-module'
   const sys = new SystemJS.constructor()
   sys.config({
@@ -15,5 +25,6 @@ test('npm-module', async _t => {
       '@*/*/package.json'
     ]
   })
+  console.log(sys.getConfig())
   await sys.import('npm-module')
 })
