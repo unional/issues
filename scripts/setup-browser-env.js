db1457378a4ce5f54d9beddb2d96acb0d7972a88
Homa Wong <homawong@gmail.com>
2017-02-09 02:42:37 -0800

    Add index.html to test on real browser.
    Still have issue working with browser-env or jsdomify

--- scripts/setup-browser-env.js
+++ scripts/setup-browser-env.js
@@ -1,4 +1,6 @@
 // require('browser-env')()
+const jsdomify = require('jsdomify').default
+jsdomify.create()
 
 // "test": "npm run clean && tsc && ava && export BROWSER_TEST=true; ava",
 // if (process.env.BROWSER_TEST) {
