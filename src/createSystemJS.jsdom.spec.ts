1c88889d06f5b6bbc537212a04d05f14cd2251b8
Homa Wong <homawong@gmail.com>
2017-02-11 16:27:53 -0800

    Use jsdom directly instead of jsdomify

--- src/createSystemJS.jsdom.spec.ts
+++ src/createSystemJS.jsdom.spec.ts
@@ -1,20 +1,27 @@
 import test from 'ava'
+import { readFileSync } from 'fs'
+import { jsdom } from 'jsdom'
 
-import jsdomify from 'jsdomify'
+const systemJSContent = readFileSync(require.resolve('systemjs'), { encoding: 'utf-8' })
 
-let createSystemJS
-test.before(() => {
-  delete require.cache[require.resolve('systemjs')]
-  jsdomify.create(`<!DOCTYPE html><html><base href="file://${process.cwd()}/index.html"></html>`)
-  createSystemJS = require('./createSystemJS').createSystemJS
+const createSystemJSContent = readFileSync(require.resolve('./createSystemJS'), { encoding: 'utf-8' })
+
+let window
+test.beforeEach(() => {
+  const document = jsdom(`<!DOCTYPE html><html><base href="file://${process.cwd()}/index.html"></html>`)
+  window = document.defaultView
+
+  let scriptEl = document.createElement('script')
+  scriptEl.textContent = systemJSContent
+  document.body.appendChild(scriptEl)
+
+  scriptEl = document.createElement('script')
+  scriptEl.textContent = createSystemJSContent
+  document.body.appendChild(scriptEl)
 })
 
-test.after(() => {
-  jsdomify.destroy()
-})
-
-test(async _t => {
-  const sys = createSystemJS('npm-module')
-  console.log(sys.getConfig())
+test.only(async _t => {
+  const sys = window.createSystemJS('npm-module')
+  // console.log(sys.getConfig())
   await sys.import('npm-module')
 })
