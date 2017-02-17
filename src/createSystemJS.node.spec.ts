6882e156a1dd2adfb91978ab5817bbe56720234f
Homa Wong <homawong@gmail.com>
2017-02-16 21:27:46 -0800

    Show for newModule issue

--- src/createSystemJS.node.spec.ts
+++ src/createSystemJS.node.spec.ts
@@ -1,13 +1,26 @@
 import test from 'ava'
 
-let createSystemJS
-test.before(() => {
-  delete require.cache[require.resolve('systemjs')]
-  createSystemJS = require('./createSystemJS').createSystemJS
+import * as SystemJS from 'systemjs'
+
+import { getLogger } from 'aurelia-logging'
+import * as Logging from 'aurelia-logging'
+
+import './createSystemJS'
+
+const log = getLogger('node spec')
+
+test.serial(async _t => {
+  const sys = new SystemJS.constructor()
+  sys.set('systemjs', SystemJS.newModule({ default: SystemJS }))
+  sys.set('aurelia-logging', SystemJS.newModule(Logging))
+  log.info('before import with { default: ... } wrap')
+  return sys.import('dist/es5/createSystemJS.js')
 })
 
-test(async _t => {
-  const sys = createSystemJS('npm-module')
-  console.log(sys.getConfig())
-  await sys.import('npm-module')
+test.serial(async _t => {
+  const sys = new SystemJS.constructor()
+  sys.set('systemjs', SystemJS.newModule(SystemJS))
+  sys.set('aurelia-logging', SystemJS.newModule(Logging))
+  log.info('before import without wrap')
+  return sys.import('dist/es5/createSystemJS.js')
 })
--- src/createSystemJS.ts
+++ src/createSystemJS.ts
@@ -1,4 +1,11 @@
+import { getLogger } from 'aurelia-logging'
 import * as SystemJS from 'systemjs'
+import sys = require('systemjs')
+
+const log = getLogger('createSysJS')
+
+log.info('require("systemjs") === import * as SystemJS', sys === SystemJS)
+log.info('SystemJS.constructor exists:', SystemJS.constructor !== undefined)
 
 export function createSystemJS(pluginName: string) {
   const sys = new SystemJS.constructor()
