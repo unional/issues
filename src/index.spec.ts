3346f0f7f1c2b74c059d39c3a20d5f3465b3c003
Homa Wong <homawong@gmail.com>
2017-02-28 00:45:16 -0800

    show no-main issue

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,14 @@
 import test from 'ava'
+import * as SystemJS from 'systemjs'
 
-test('first test', t => {
-  t.pass('no test yet')
+test('first test', async _t => {
+  const sys = new SystemJS.constructor()
+  sys.config({
+    baseURL: 'node_modules',
+    packageConfigPaths: [
+      '*/package.json',
+      '@*/*/package.json'
+    ]
+  })
+  await sys.import('make-error')
 })
