de4bf091cc2350e20ee6033c83c90ae446e78dcd
Homa Wong <homawong@gmail.com>
2017-03-04 15:20:20 -0800

    Show nyc-jsdom-systemjs issue

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,42 @@
 import test from 'ava'
+import * as fileUrl from 'file-url'
+import { env, createVirtualConsole } from 'jsdom'
+
+import { foo } from './index'
 
 test('first test', t => {
-  t.pass('no test yet')
+  const virtualConsole = createVirtualConsole().sendTo(console)
+  return new Promise<any>((resolve, reject) => {
+    env({
+      html: '<br>',
+      url: fileUrl(process.cwd()) + '/',
+      virtualConsole,
+      scripts: [
+        require.resolve('systemjs')
+      ],
+      done(err, win) {
+        if (err) {
+          reject(err)
+        }
+        else {
+          resolve(win)
+        }
+      }
+    })
+  }).then(async win => {
+    const sys = win.SystemJS
+    sys.config({
+      packages: {
+        'dist/es5': {
+          defaultExtension: 'js'
+        }
+      }
+    })
+    const m = await sys.import('dist/es5/index')
+    t.is(m.boo(), 'boo')
+  })
+})
+
+test(t => {
+  t.is(foo(), 'foo')
 })
