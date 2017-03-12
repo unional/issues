ba3ffb806cf7bca28a8354b7174d400c9bac105d
Homa Wong <homawong@gmail.com>
2017-03-12 01:57:44 -0800

    Show order issue

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,23 @@
 import test from 'ava'
 
+import { Container, singleton } from 'aurelia-dependency-injection'
+
+@singleton()
+class Depender {
+  constructor(private dep: Dependent) { }
+  get() {
+    return this.dep.foo
+  }
+}
+
+class Dependent {
+  foo = 'foo'
+}
+
 test('first test', t => {
-  t.pass('no test yet')
+  const container = new Container()
+
+  const actual = container.get(Depender)
+
+  t.is(actual.get(), 'foo')
 })
