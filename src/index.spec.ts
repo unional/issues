00cf5b59a38d0cc03a360c0e0106e921c0d41b33
Homa Wong <homawong@gmail.com>
2017-03-16 20:12:30 -0700

    Add child type issue

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,24 +2,35 @@ import test from 'ava'
 import fileUrl = require('file-url')
 import { env, createVirtualConsole } from 'jsdom'
 
-test('first test', t => {
+let win: any
+test.before(() => {
   return new Promise((resolve, reject) => {
     const virtualConsole = createVirtualConsole().sendTo(console)
     env({
       html: '<br>',
       url: fileUrl(process.cwd()) + '/',
       virtualConsole,
-      done(err, win: any) {
+      done(err, w: any) {
         if (err) {
           reject(err)
         }
 
-        const xml = new win.DOMParser().parseFromString('<script>x<\/script>', 'text/xml')
-        const parseError = xml.getElementsByTagName('parsererror')
-        console.log(parseError)
-        t.is(parseError.length, 0)
+        win = w
         resolve()
       }
     })
   })
+})
+
+test('parseerror', t => {
+  const xml = new win.DOMParser().parseFromString('<script>x</script>', 'text/xml')
+  const parseError = xml.getElementsByTagName('parsererror')
+  console.log(parseError)
+  t.is(parseError.length, 0)
+
+})
+
+test('wrong child type', t => {
+  const root = new win.DOMParser().parseFromString('<?xml version=\"1.0\"?>\n<a><\/a>\n', 'text/xml')
+  t.is(root.firstChild.nodeType, 1)
 })
